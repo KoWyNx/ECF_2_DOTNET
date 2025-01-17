dff2a660d063409d9bd444b4e16bdfc249007957
KoWyNx <fberrani@hotmail.fr>
2025-01-17 09:23:01 +0100

    first commit

---
+++ Context/MyDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace EcfDotnet.Context
+{
+    public partial class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
